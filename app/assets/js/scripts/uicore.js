a1c561aedff6c23df9e62097c752136d13527eb4
BabyBear7 <soul7687@gmail.com>
2025-12-04 06:02:28 +0900

    Update uicore.js

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -49,7 +49,7 @@ if(!isDev){
                 loggerAutoUpdater.info('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/aaaawsq/jellypia1/releases/download/v${info.version}/LauncherDemo-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
+                    info.darwindownload = `https://github.com/aaaawsq/jellypia1/releases/download/v${info.version}/jellypia-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
                     showUpdateUI(info)
                 }
                 
